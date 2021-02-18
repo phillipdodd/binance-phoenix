23198357c84b50f648932672bd9a2d0a8e773643
Phillip Dodd <phillipdodd@gmail.com>
2021-02-18 11:43:39 -0500

    exports class itself

--- ticker.js
+++ ticker.js
@@ -1,16 +1,21 @@
-module.exports = binanceInstance => new Ticker(binanceInstance);
+const logger = require("./lib/myWinston")("ticker");
 
 class Ticker {
     
     constructor(binanceInstance) {
-        this.binanceInstance = binanceInstance;
+        this.binanceInstance = binanceInstance.client;
         this.data = {};
     }
-
-    setWebsocket(symbolArray) {
-        if (this.websocket) this.websocket();
-        this.websocket = this.binanceInstance.ws.ticker(symbolArray, (tickerData) => {
-            this.data[tickerData.symbol] = tickerData;
-        });
+    
+    async setWebsocket(symbolArray) {
+        try {
+            if (this.websocket) this.websocket();
+            this.websocket = this.binanceInstance.ws.ticker(symbolArray, (tickerData) => {
+                this.data[tickerData.symbol] = tickerData;
+            });
+        } catch (e) {
+            logger.error(e.message);
+        }
     }
 }
+module.exports = Ticker;
