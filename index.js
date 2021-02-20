69c9638d08f5031d5a2741e7d3ae3c7e584c2631
Phillip Dodd <phillipdodd@gmail.com>
2021-02-20 17:42:06 -0500

    makes use of import keyword

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-require("dotenv").config();
-const Instance = require('./instance');
-const SimpleMovingAverageTracker = require('./indicators/simpleMovingAverageTracker');
+import { Instance } from './Instance.js';
+import { Users } from './Constants.js';
+import { tomStrategy } from './Strategies.js';
 
-const instanceTom = new Instance({
-    apiKey: process.env.API_KEY_TOM,
-    apiSecret: process.env.API_SECRET_TOM,
-    strategy: require('./strategies/tomStrategy.json')
-});
-// instanceTom.init();
+// const Instance = require('./instance');
+// const SimpleMovingAverageTracker = require('./indicators/simpleMovingAverageTracker');
 
-const sma7 = new SimpleMovingAverageTracker("DOGEUSD", "1m", 5, instanceTom.client);
-(async () => { 
-    await sma7.init();
-    setInterval(() => {
-        console.log(sma7.sma);
-    }, 1000);
-})()
+const instanceTom = new Instance(
+    process.env.API_KEY_TOM,
+    process.env.API_SECRET_TOM,
+    Users.Tom,
+    1.005,
+    tomStrategy
+);
+
+instanceTom.init();
+
+// const sma7 = new SimpleMovingAverageTracker("DOGEUSD", "1m", 5, instanceTom.client);
+// (async () => { 
+//     await sma7.init();
+//     setInterval(() => {
+//         console.log(sma7.sma);
+//     }, 1000);
+// })()
 
 // setInterval(() => {
 //     console.log(sma7.sma);
