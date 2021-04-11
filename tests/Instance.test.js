a2170faab0ec9dfdad9d216c78e4f0450803b096
Phillip Dodd <phillipdodd@gmail.com>
2021-04-11 17:18:09 -0400

    init wip

---
+++ tests/Instance.test.js
@@ -0,0 +1,8 @@
+// const Instance = require('../Instance.js');
+// const Users = require('../data/Users.js');
+// const { tomStrategy } = require("../data/Strategies.js");
+// const { indexTransformDependencies } = require('mathjs');
+
+// const instance = new Instance(User.Tom, tomStrategy);
+
+
