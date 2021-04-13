36d57d731f6f7a8cbe6c1f7f588b40efa372d660
Phillip Dodd <phillipdodd@gmail.com>
2021-04-13 14:53:42 -0400

    started

--- tests/Instance.test.js
+++ tests/Instance.test.js
@@ -1,11 +1,22 @@
-// const Instance = require('../Instance.js');
-// const Users = require('../data/Users.js');
-// const { tomStrategy } = require("../data/Strategies.js");
-// const { indexTransformDependencies } = require('mathjs');
+const Instance = require('../Instance.js');
+const Users = require('../data/Users.js');
+const { tomStrategy } = require("../data/Strategies.js");
 
-// const instance = new Instance(User.Tom, tomStrategy);
+const instance = new Instance(Users.Tom, tomStrategy);
 
-test.todo('Can initialize successfully');
-test.todo('Can open websocket');
+it('Can successfully create an instance', () => {
+    expect(instance).toBeDefined();
+    expect(instance.client).toBeDefined();
+    expect(instance.dataHandler).toBeDefined();
+    expect(instance.utility).toBeDefined();
+});
+
+it('Can successfully open/close a user websocket', async () => {
+    const clean = await instance.client.ws.user((eventData) => { });
+    expect(clean).toBeDefined();
+    clean();
+    console.log(clean);
+});
+
 
 
