68e45190f87d97bc789408766f9b1daf7ccf084b
Phillip Dodd <phillipdodd@gmail.com>
2021-04-15 11:19:39 -0400

    corrected

--- tests/Instance.test.js
+++ tests/Instance.test.js
@@ -6,7 +6,7 @@ const instance = new Instance(Users.Tom, tomStrategy);
 
 it('Can successfully create an instance', () => {
     expect(instance).toBeDefined();
-    expect(instance).toHaveProperty(client);
+    expect(instance).toHaveProperty('client');
 });
 
 it('Can successfully open/close a user websocket', async () => {
